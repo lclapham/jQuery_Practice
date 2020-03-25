dbb40b887688ebb01ff2b0b25cb31927e32c4684
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-03-24 22:10:11 -0700

    super basic jQuery examples

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//$('#lead-banner p');
+
+$('h3').css({border: "3px solid blue"});
+
+$('.wrapper').css({border: "3px solid red"});
+
+document.getElementById("logo").style.color = "red";
+
+$('#clients').css({border: "3px solid yellow"});
