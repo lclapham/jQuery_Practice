a8867b790e703dad1716ece07f7ac238f5bec3b2
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-03-25 17:43:49 -0700

    Completed lesson 7

--- js/scripts.js
+++ js/scripts.js
@@ -33,4 +33,21 @@
 //$("#contact-methods").next().css({border: "2px solid blue"});
 
 // You could also do previous, which would get the element before the element you specific
-$("#social-nav").prev().css({border: "2px solid blue"});
+//$("#social-nav").prev().css({border: "2px solid blue"});
+
+// If you wanted to get the parent of an element
+//$(".banner-title").parent().css({border: "2px solid blue"});
+
+//If you make parent plural. you get all of the parent elements.
+
+//$(".banner-title").parents().css({border: "2px solid blue"});
+
+//Get all f the childern elements
+//$("#social-nav").children().css({border: "2px solid blue"});
+
+//This will find all of the instances of class=facebook within the element id="contact"
+//$("#contact").find(".facebook").css({border: "2px solid blue"});
+
+//This will find the element with a clsss of "wrapper closest to the element ID="social-nav". It moves up the DOM to find it
+$("#social-nav").closest(".wrapper").css({border: "3px solid red"});
+
